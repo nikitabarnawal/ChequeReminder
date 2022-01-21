53910f5a9f46202c46cce7a536d377f119873193
NikitaBarnwal <nikitabarnwal@Nikitas-MacBook-Pro.local>
2022-01-21 20:05:54 +0530

    added contacts

--- App.js
+++ App.js
@@ -2,17 +2,46 @@
 
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import TabBar from './components/common/TabBar';
+import Icons from 'react-native-vector-icons/Ionicons';
+import Home from './components/screens/Home';
+import Money from './components/screens/Money';
+import Contacts from './components/screens/Contacts';
+import Settings from './components/screens/Settings';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="TabBar" component={TabBar} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'home'
+                : 'home-outline';
+            } else if (route.name === 'Money') {
+              iconName = focused ? 'card' : 'card-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'settings-sharp' : 'settings-outline';
+            }
+            // You can return any component that you like here!
+            return <Icons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Money" component={Money} />
+        <Tab.Screen name="Settings" component={Settings} />
+        <Stack.Screen name="Contacts" component={Contacts} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
--- components/screens/Home.js
+++ components/screens/Home.js
@@ -1,14 +1,35 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, TouchableHighlight, View, StyleSheet, Alert } from 'react-native';
 
-
-function Home() {
+function Home({ navigation }) {
+    const onPress = () => navigation.navigate('Contacts');
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Home!</Text>
+        <View style={styles.container}>
+            <TouchableHighlight style={styles.button} onPress={onPress}>
+                <Text>Add Customers</Text>
+            </TouchableHighlight>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    button: {
+        justifyContent: "flex-end",
+        alignItems: "flex-end",
+        backgroundColor: "#32a852",
+        width: "auto",
+        marginBottom: 10,
+        marginRight: 5,
+        borderRadius: 8,
+        padding: 10,
+        marginTop: "auto",
+        marginLeft: "auto"
+
+    }
+});
 
 
 export default Home;
